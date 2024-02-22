10e3e5fe1ce70bee99fedc2ee8895a7030eadaa2
Purshottam Nepal <prunepal3339@gmail.com>
2024-02-22 07:18:12 +0545

    Remove comment from Routes

--- app/Routes.php
+++ app/Routes.php
@@ -20,15 +20,6 @@ class Routes
     $router->add('get', '/login', fn() => 'Login Page');
     $router->add('post', '/login',fn() => 'Login Post Page');
     $router->add('get', '/internal-server-error', fn() => throw new \Exception("Internal Server Error"));
-    // $router
-    //     ->add('get', '/customers/{id}',
-    //        function() use ($router) {
-    //          $params = $router->currentRoute()->routeParams();
-    //          var_dump($params);
-    //          return "Customer {$params['id']}"; 
-    //       })
-    //       ->name('app_customer_index');
-    // ;
     $router->add('get', '/customers/{id}', [CustomerController::class, 'showCustomerAction']);
     $router->add('get', '/products/{id}', [ProductController::class, 'showProductAction']);
     $router->errorHandler(404, fn() => "Page Not found!");
